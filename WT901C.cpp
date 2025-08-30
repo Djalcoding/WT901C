d8baa8f277777180708c787a30064f0f9beb88b1
Djal Bertrand <100029664+Djalcoding@users.noreply.github.com>
2025-08-30 14:14:50 -0400

    Moved doc and polished code, ready for release

--- WT901C.cpp
+++ WT901C.cpp
@@ -2,10 +2,6 @@
 #include<Arduino.h>
 #include<HardwareSerial.h>
 #include<stdio.h>
-#include<string>
-// The WT901C constructor
-// #### Parameters : 
-// - port : the serial port connected to the sensor, is 2 by default.
 WT901C::WT901C(byte port) {
     switch (port)
     {
@@ -17,107 +13,52 @@ WT901C::WT901C(byte port) {
     }
 }
 
-// The WT901C sensor constructor
-// #### Parameters : 
-// - serialPort : a pointer to the serial port connected to the sensor.
-// #### Note :
-// you do not need and should not start the serial port and should use WT901C::begin() instead
 WT901C::WT901C(HardwareSerial* serialPort) : SerialWIT(serialPort) {}
 
-//Get the X component of the current acceleration of the sensor in g (9.8067 m/s²).
-// ### Limits : 
-// The acceleration range is ±16g
 double WT901C::AccelerationReading::getX(){return Ax;}
-//Get the Y component of the current acceleration of the sensor in g (9.8067 m/s²).
-// ### Limits : 
-// The acceleration range is ±16g
 double WT901C::AccelerationReading::getY() { return Ay;}
-//Get the Z component of the ccurent acceleration of the sensor in g (9.8067 m/s²).
-// ### Limits : 
-// The acceleration range is ±16g
 double WT901C::AccelerationReading::getZ() {return Az;}
-// Returns a char array that is formatted in this format : 
-// X : xValue m/s, Y : yValue m/s2, Z : zValue m/s2
+
 char* WT901C::AccelerationReading::format() {
     static char formatted[64];
     std::sprintf(formatted, "X : %.4f m/s2, Y : %.4f m/s2, Z : %.4f m/s2", getX(), getY(), getZ());
     return formatted;
 }
 
-//Get the X(roll) component of the current angular velocity of the sensor in degrees per seconds.
-// ### Limits : 
-// The angular velocity of all angles have a range of ±2000°/s 
 double WT901C::AngularVelocityReading::getX(){return Wx;}
-//Get the Y(pitch) component of the current angular velocity of the sensor in degrees per seconds.
-// ### Limits : 
-// The angular velocity of all angles have a range of ±2000°/s 
 double WT901C::AngularVelocityReading::getY() { return Wy;}
-//Get the Z(yaw) component of the current angular velocity of the sensor in degrees per seconds.
-// ### Limits : 
-// The angular velocity of all angles have a range of ±2000°/s 
 double WT901C::AngularVelocityReading::getZ() {return Wz;}
 
-// Returns a char array that is formatted in this format : 
-// X : xValue deg/s, Y : yValue deg/s, Z : zValue deg/s
 char* WT901C::AngularVelocityReading::format() {
     static char formatted[64];
     std::sprintf(formatted, "X : %.4f deg/s, Y : %.4f deg/s, Z : %.4f deg/s", getX(), getY(), getZ());
     return formatted;
 }
 
-//Get the X(roll) component of the current angle of the sensor in degrees, represented as euler angles(X,Y,Z).
-// ### Limits : 
-// The angle components have a range of ±180 degrees
 double WT901C::RotationReading::getX(){return Rx;}
-//Get the Y(pitch) component of the current angle of the sensor in degrees, represented as euler angles(X,Y,Z).
-// ### Limits : 
-// The angle components have a range of ±180 degrees
 double WT901C::RotationReading::getY() { return Ry;}
-//Get the Z(yaw) component of the current angle of the sensor in degrees, represented as euler angles(X,Y,Z).
-// ### Limits : 
-// The angle components have a range of ±180 degrees
 double WT901C::RotationReading::getZ() {return Rz;}
 
-// Returns a char array that is formatted in this format : 
-// X : xValue deg, Y : yValue deg, Z : zValue deg
 char* WT901C::RotationReading::format() {
     static char formatted[64];
     std::sprintf(formatted, "X : %.4f deg, Y : %.4f deg, Z : %.4f deg", getX(), getY(), getZ());
     return formatted;
 }
 
-//Get the X component of the current magnetfic field strength of the sensor in Gauss.
-// ### Limits : 
-// The magnetic components have a range of ±2Gauss
 double WT901C::MagnetometerReading::getX(){return Mx;}
-//Get the Y component of the current magnetfic field strength of the sensor in Gauss.
-// ### Limits : 
-// The magnetic components have a range of ±2Gauss
 double WT901C::MagnetometerReading::getY() { return My;}
-//Get the Z component of the current magnetfic field strength of the sensor in Gauss.
-// ### Limits : 
-// The magnetic components have a range of ±2Gauss
 double WT901C::MagnetometerReading::getZ() {return Mz;}
-// Returns a char array that is formatted in this format : 
-// X : xValue G, Y : yValue G, Z : zValue G
+
 char* WT901C::MagnetometerReading::format() {
     static char formatted[64];
     std::sprintf(formatted, "X : %.4f G, Y : %.4f G, Z : %.4f G", getX(), getY(), getZ());
     return formatted;
 }
 
-// The method to initialize communication with the sensor
-// #### Parameters
-// - baud        : the baud rate represented as an integer, by defaut, this is usually 9600 bauds but the sensor could have been configured to use a diffenred baud rate.
-// - receiver    : the receiver pin ID, usually 16, if on serial port 2
-// - transmitter : the transmitter pin ID, usually 17, if on serial port 2
-// #### NOTE
-// THIS WILL ONLY COMPILE ON A ESP-32
 void WT901C::begin(int baud, int receiver, int transmitter){
     SerialWIT->begin(baud, SERIAL_8N1, receiver, transmitter);
 }
 
-// The method to update the information given by the sensor, this should be called periodicly
 void WT901C::update(){
 
         while(SerialWIT->available()){
@@ -170,24 +111,20 @@ void WT901C::update(){
         }
 }
 
-//return the acceleration readings obtained from the sensor
 WT901C::AccelerationReading WT901C::getAccelerationReading(){
     return currentAcceleration;
 }
-//return the angular velocity readings obtained from the sensor
+
 WT901C::AngularVelocityReading WT901C::getAngularVelocityReading(){
     return currentAngularVelocity;
 }
 
-//return the rotation readings obtained from the sensor
 WT901C::RotationReading WT901C::getRotationReading(){
     return currentRotation;
 }
-//return the magnetic field readings obtained from the sensor
 WT901C::MagnetometerReading WT901C::getMagneticFieldReading(){
     return currentMagneticReading;
 }
-//return the current temperature of the sensor
 double WT901C::getTemperatureReading(){
     return temperature;
 }
--- WT901C.h
+++ WT901C.h
@@ -1,12 +1,28 @@
 #include<Arduino.h>
 #include<HardwareSerial.h>
-#include<string>
 class WT901C{
   public:
+    // The WT901C constructor
+    // #### Parameters : 
+    // - port : the serial port connected to the sensor, is 2 by default.
   WT901C(byte port);
+    // The WT901C constructor
+    // #### Parameters : 
+    // - serialPort : a pointer to the serial port connected to the sensor.
+    // #### Note :
+    // you do not need and should not start the serial port and should use WT901C::begin() instead
   WT901C(HardwareSerial* serial);
 
+// The method to initialize communication with the sensor
+// #### Parameters
+// - baud        : the baud rate represented as an integer, by defaut, this is usually 9600 bauds but the sensor could have been configured to use a diffenred baud rate.
+// - receiver    : the receiver pin ID, usually 16, if on serial port 2
+// - transmitter : the transmitter pin ID, usually 17, if on serial port 2
+// #### NOTE
+// THIS WILL ONLY COMPILE ON A ESP-32
   void begin(int baud, int rx, int tx);
+
+// The method to update the information given by the sensor, this should be called periodicly
   void update();
 
   // A struct that contains the three variables(x,y,z) of acceleration
@@ -14,9 +30,24 @@ class WT901C{
     double Ax;
     double Ay;
     double Az;
+
+    //Get the X component of the current acceleration of the sensor in g (9.8067 m/s²).
+    // ### Limits : 
+    // The acceleration range is ±16g
     double getX();
+
+    //Get the Y component of the current acceleration of the sensor in g (9.8067 m/s²).
+    // ### Limits : 
+    // The acceleration range is ±16g
     double getY();
+
+    //Get the Z component of the ccurent acceleration of the sensor in g (9.8067 m/s²).
+    // ### Limits : 
+    // The acceleration range is ±16g
     double getZ();
+
+    // Returns a char array that is formatted in this format : 
+    // X : xValue m/s, Y : yValue m/s2, Z : zValue m/s2
     char* format();
   };
 
@@ -25,9 +56,24 @@ class WT901C{
     double Wx;
     double Wy;
     double Wz;
+
+    //Get the X(roll) component of the current angular velocity of the sensor in degrees per seconds.
+    // ### Limits : 
+    // The angular velocity of all angles have a range of ±2000°/s 
     double getX();
+
+    //Get the Y(pitch) component of the current angular velocity of the sensor in degrees per seconds.
+    // ### Limits : 
+    // The angular velocity of all angles have a range of ±2000°/s 
     double getY();
+
+    //Get the Z(yaw) component of the current angular velocity of the sensor in degrees per seconds.
+    // ### Limits : 
+    // The angular velocity of all angles have a range of ±2000°/s 
     double getZ();
+
+    // Returns a char array that is formatted in this format : 
+    // X : xValue deg/s, Y : yValue deg/s, Z : zValue deg/s
     char* format();
   };
 
@@ -36,9 +82,24 @@ class WT901C{
     double Rx;
     double Ry;
     double Rz;
+
+    //Get the X(roll) component of the current angle of the sensor in degrees, represented as euler angles(X,Y,Z).
+    // ### Limits : 
+    // The angle components have a range of ±180 degrees
     double getX();
+
+    //Get the Y(pitch) component of the current angle of the sensor in degrees, represented as euler angles(X,Y,Z).
+    // ### Limits : 
+    // The angle components have a range of ±180 degrees
     double getY();
+
+    //Get the Z(yaw) component of the current angle of the sensor in degrees, represented as euler angles(X,Y,Z).
+    // ### Limits : 
+    // The angle components have a range of ±180 degrees
     double getZ();
+
+    // Returns a char array that is formatted in this format : 
+    // X : xValue deg, Y : yValue deg, Z : zValue deg
     char* format();
   };
 
@@ -47,16 +108,35 @@ class WT901C{
     double Mx;
     double My;
     double Mz;
+    //Get the X component of the current magnetfic field strength of the sensor in Gauss.
+    // ### Limits : 
+    // The magnetic components have a range of ±2Gauss
     double getX();
+
+    //Get the Y component of the current magnetfic field strength of the sensor in Gauss.
+    // ### Limits : 
+    // The magnetic components have a range of ±2Gauss
     double getY();
+
+    //Get the Z component of the current magnetfic field strength of the sensor in Gauss.
+    // ### Limits : 
+    // The magnetic components have a range of ±2Gauss
     double getZ();
+
+    // Returns a char array that is formatted in this format : 
+    // X : xValue G, Y : yValue G, Z : zValue G
     char* format(); 
   };
 
+//return the acceleration readings obtained from the sensor
   AccelerationReading getAccelerationReading();
+//return the rotation readings obtained from the sensor
   RotationReading getRotationReading();
+//return the magnetic field readings obtained from the sensor
   MagnetometerReading getMagneticFieldReading();
+//return the angular velocity readings obtained from the sensor
   AngularVelocityReading getAngularVelocityReading();
+//return the current temperature of the sensor
   double getTemperatureReading();
 
   private:
